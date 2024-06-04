cf6390b6ea9bdc640c128ee5d7d470be55bdcaa5
josue <jo.olopes007@gmail.com>
2024-06-04 14:25:21 -0300

    banco de dados e outras coisas

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'  # ou o endereço do seu banco de dados MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'  # ou o endereço do seu banco de dados MySQL
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '32001josue@A'
-app.config['MYSQL_DB'] = ''
+app.config['MYSQL_DB'] = 'teste'
 mysql = MySQL(app)
 
 @app.route('/')
@@ -41,6 +41,17 @@ def cart():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def add_login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (email, senha) VALUES (%s, %s)", (email, senha))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
